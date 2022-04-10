c7dac1f39cd99064360ab5f84cc31d9102614c1f
Ian Marcos <ian.marcos.ortiz@gmail.com>
2022-04-10 13:22:36 -0500

    POST operation completed

--- js/index.js
+++ js/index.js
@@ -1,21 +1,45 @@
-addEventListener('DOMContentLoaded', loadJSON);
+// addEventListener('DOMContentLoaded', loadSelectors);
+addEventListener('submit',handleSubmit);
 
-async function loadJSON() {
-  try {
-    const response = await fetch('https://clothes-api-ian.herokuapp.com/api/products/all?limit=1');
-    const results = await response.json();
-    if(results.status !== 200){
-      console.log('Something went wrong fetching the product');
-      return;
-    }
-    console.log(results);
-    
-  } catch (error) {
-    console.log(error);
+const responseBlock = document.querySelector('.response-block');
+const jsonCode = document.querySelector('#json-response');
+/**In case field validation is required
+  const formName = document.querySelector('#form-name');
+  const formDescription = document.querySelector('#form-desc');
+  const formPrice = document.querySelector('#form-price');
+  const formDiscount = document.querySelector('#form-discount');
+  const formCountry = document.querySelector('#form-country');
+  const formImages = document.querySelector('#form-prodImg');
+*/
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const productJSON = await createProduct();
+  if( productJSON ){
+    productJSON.results.status === 201 ? responseBlock.classList.add('success-shadow') : responseBlock.classList.add('error-shadow');
+    jsonCode.innerText = JSON.stringify(productJSON, null, 2);
   }
 }
 
-async function createProduct(e) {
-  e.preventDefault();
-  
+async function createProduct() {
+  try {
+    const response = await fetch('https://clothes-api-ian.herokuapp.com/api/products/new', {
+      method: "POST",
+      // headers: {
+      //   "Content-Type": "form-data"
+      // },
+      body: new FormData(document.querySelector('#product-form'))
+    });
+
+    const results = await response.json();
+    
+    if(results.status === 500){
+      console.log('Server Error');
+      return undefined;
+    }
+
+    return results;
+  } catch (error) {
+      return undefined;
+  }
 }
